5a45324d1d57d81781ea8ec6b077aa716ff9b34f
Marat Zhanpeisov <themjdex@gmail.com>
2022-05-18 10:40:47 +0500

    file system work, save token

---
+++ weather.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+import { getArgs } from './helpers/args.js';
+import { printHelp, printSuccess, printError } from './services/log.service.js';
+import { saveKeyValue } from './services/storage.service.js';
+
+const saveToken = async (token) => {
+	try {
+		await saveKeyValue('token', token);
+		printSuccess('Токен сохранен');
+	} catch (e) {
+		printError(e.message);
+	}
+}
+
+const initCLI = () => {
+	const args = getArgs(process.argv);
+	if (args.h) {
+		printHelp();
+	}
+	if (args.s) {
+
+	}
+	if (args.t) {
+		return saveToken(args.t);
+	}
+	// show wheather
+};
+
+initCLI();
